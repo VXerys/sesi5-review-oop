fc61ad4740de0fbaf82ec7fb574cda2fccd73e40
SehanAF <sehanaf4@gmail.com>
2025-03-13 16:51:30 +0700

    Update: hapus komentar

--- src/appBank.java
+++ src/appBank.java
@@ -7,36 +7,25 @@ public class appBank {
 
           System.out.println("Selamat Datang di Bank XYZ");
           
-          // Rekening rekening = new Rekening("Akbar Dokoni", "12345678", 0);
-          // rekening.tampilkanInfo();
-          // System.out.println("Setor : " + rekening.addSetor(100));
-          // System.out.println("Tarik : " + rekening.addTarik(50));
-          // System.out.println("Setor : " + rekening.addSetor(10));
-          // rekening.tampilkanInfo();
-          // System.out.println("================================================================");
-
-
+          Rekening rekening = new Rekening("Akbar Dokoni", "12345678", 0);
+          rekening.tampilkanInfo();
+          System.out.println("Setor : " + rekening.addSetor(100));
+          System.out.println("Tarik : " + rekening.addTarik(50));
+          System.out.println("Setor : " + rekening.addSetor(10));
+          rekening.tampilkanInfo();
+          System.out.println("================================================================");
          
-          // Tabungan tabungan = new Tabungan("Resandi Alfa", "12345678", 0, 0.5);
-          // System.out.println("Setor : " + tabungan.addSetor(200));
-          // System.out.println("Tarik : " + tabungan.addTarik(100));
-          // tabungan.tampilkanInfo();
-          // System.out.println("================================================================");
-
-          // Giro giro = new Giro("Sehan Alfarisi", "12345678", 0, 500000);
-          // System.out.println("Setor : " + giro.addSetor(100));
-          // System.out.println("Tarik : " + giro.addTarik(50));
-          // System.out.println("Setor : " + giro.addSetor(20));
-          // giro.tampilkanInfo();
+          Tabungan tabungan = new Tabungan("Resandi Alfa", "12345678", 0, 0.5);
+          System.out.println("Setor : " + tabungan.addSetor(200));
+          System.out.println("Tarik : " + tabungan.addTarik(100));
+          tabungan.tampilkanInfo();
+          System.out.println("================================================================");
 
           Giro giro = new Giro("Sehan Alfarisi", "12345678", 0, 50000);
           System.out.println("Setor : " + giro.addSetor(100));
           System.out.println("Tarik : " + giro.addTarik(50));
           System.out.println("Setor : " + giro.addSetor(20));
           giro.tampilkanInfo();
-
-          
-
           
      }
 }
